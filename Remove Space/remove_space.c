28b94fbb029ed9a4a2e731367a3305d198029205
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-05-26 16:54:27 +0600

    remove space

---
+++ Remove Space/remove_space.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+
+    FILE *in = fopen("input.c", "r");
+    FILE *out = fopen("output.txt", "w");
+
+    if(in == NULL){
+        printf("Error Cannot open input file\n");
+        return 1;
+    }
+
+    while((ch = fgetc(in)) != EOF){
+        if(ch != ' ' && ch != '\t' && ch != '\n'){
+            fputc(ch, out);
+        }
+    }
+
+    fclose(in);
+    fclose(out);
+
+    printf("Spcaes removed successfully. Please check output.txt\n");
+
+
+    return 0;
+}
